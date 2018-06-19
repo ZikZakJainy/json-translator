6d8597e35020cca6a2433e1f840322be46d84ed0
Shubham Jain <shubham@pietechsolution.com>
2018-06-19 12:50:56 +0530

    verification.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const Translate = require('@google-cloud/translate');
+// const Translate = require('@google-cloud/translate');
 const express = require('express')
 const app = express();
-// import mongoose from 'mongoose';
-var mongoose=require('mongoose');
-// import expressJwt from 'express-jwt';
-const expressJwt=require('express-jwt');
+// // import mongoose from 'mongoose';
+// var mongoose=require('mongoose');
+// // import expressJwt from 'express-jwt';
+// const expressJwt=require('express-jwt');
 var cors = require('cors');
 app.use(cors());
 // mongoose.Promise = global.Promise;
 // mongoose.connect("mongodb://root:root@ds135917.mlab.com:35917/test3");
-mongoose.Promise = require('bluebird');
+// mongoose.Promise = require('bluebird');
 
-const LocalStrategy = require('passport-local').Strategy;
+// const LocalStrategy = require('passport-local').Strategy;
 
 
 var bodyParser = require('body-parser');
@@ -19,10 +19,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-// var sourceJSON = require('./wordsJSON.json');
-const translate = new Translate({
-  keyFilename: './serviceaccount.json'
-});
+// // var sourceJSON = require('./wordsJSON.json');
+// const translate = new Translate({
+//   keyFilename: './serviceaccount.json'
+// });
 
 // var nameSchema = new mongoose.Schema({
 //   username: String,
@@ -43,41 +43,41 @@ const translate = new Translate({
 // });
 
 // Connect to MongoDB
-mongoose.connect('mongodb://root:root@ds135917.mlab.com:35917/test3');
-mongoose.connection.on('error', function(err) {
-  console.error(`MongoDB connection error: ${err}`);
-  process.exit(-1); // eslint-disable-line no-process-exit
-});
+// mongoose.connect('mongodb://root:root@ds135917.mlab.com:35917/test3');
+// mongoose.connection.on('error', function(err) {
+//   console.error(`MongoDB connection error: ${err}`);
+//   process.exit(-1); // eslint-disable-line no-process-exit
+// });
 
 // require('./routes').default(app);
 // var target = 'ru';
 app.get('/api/emails', function (req, res) {
     res.json({status: 'GET /api/users'});
 });
-app.post('/translate/:target', (req, res) => {
-//   console.log('hitting...'+JSON.stringify(req.params.target));
-// console.log('hitting...'+JSON.stringify(req.body));
-var sourceJSON = req.body.sourceJSON;
-  var sourceKeyArry = Object.keys(sourceJSON);
-  var sourceValArry = Object.values(sourceJSON);
-  console.log('key '+JSON.stringify(sourceKeyArry));
-  console.log('val '+JSON.stringify(sourceValArry));
-  // console.log('hitting...key'+JSON.stringify(sourceKeyArry));
-  // console.log('hitting...values'+JSON.stringify(sourceKeyArry));
-  translate.translate(sourceValArry, req.params.target).then(results => {
-    const translation = results[0];
-    console.log(translation);
-    res.send(arry2obj(sourceKeyArry, translation));
-  }).catch(err => {
-    console.error('ERROR:', err);
-  });
-});
-app.listen(2800, () => console.log('Translator app listening on port 2800!'));
+// app.post('/translate/:target', (req, res) => {
+// //   console.log('hitting...'+JSON.stringify(req.params.target));
+// // console.log('hitting...'+JSON.stringify(req.body));
+// var sourceJSON = req.body.sourceJSON;
+//   var sourceKeyArry = Object.keys(sourceJSON);
+//   var sourceValArry = Object.values(sourceJSON);
+//   console.log('key '+JSON.stringify(sourceKeyArry));
+//   console.log('val '+JSON.stringify(sourceValArry));
+//   // console.log('hitting...key'+JSON.stringify(sourceKeyArry));
+//   // console.log('hitting...values'+JSON.stringify(sourceKeyArry));
+//   translate.translate(sourceValArry, req.params.target).then(results => {
+//     const translation = results[0];
+//     console.log(translation);
+//     res.send(arry2obj(sourceKeyArry, translation));
+//   }).catch(err => {
+//     console.error('ERROR:', err);
+//   });
+// });
+ app.listen(2800, () => console.log('Translator app listening on port 2800!'));
 
-function arry2obj(sourceKeyArry, translation) {
-  var destination = {};
-  for (var i = 0; i < sourceKeyArry.length; i++) {
-    if (sourceKeyArry[i] !== undefined) destination[sourceKeyArry[i]] = translation[i];
-  }
-  return destination;
-}
+// function arry2obj(sourceKeyArry, translation) {
+//   var destination = {};
+//   for (var i = 0; i < sourceKeyArry.length; i++) {
+//     if (sourceKeyArry[i] !== undefined) destination[sourceKeyArry[i]] = translation[i];
+//   }
+//   return destination;
+// }
